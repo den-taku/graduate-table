0712b98c931ab4a6d186a40a7f599eda9fca53be
den-taku <dentaku1412@gmail.com>
2022-02-01 23:37:43 +0900

    Implement

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,29 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn calc(
-    mut values: std::str::SplitWhitespace,
-    opt: &mut usize,
-    infeasible: &mut usize,
-    avr: &mut usize,
-    sum: &mut u128,
-    max: &mut u128,
-    gap_lb: &mut usize,
-    gap_ub: &mut usize,
-    nodes: &mut u128,
-    cached: &mut u128,
-    oom: &mut usize,
-    fail_count: &mut usize,
-) {
-    if values.next() == Some("0") {
-        // oom
-        *oom += 1;
-        return;
-    }
-    if values.next() == Some("0") {
-        // time limit
-        return;
-    }
-    *opt += 1;
-    if let Some(time) = values.next() {
-        let time = time.parse::<u128>().unwrap();
-        *sum += time;
-        *max = std::cmp::max(*max, time);
-    }
-    if values.next() == Some("0") {
-        // no feasible answer
-        *infeasible += 1;
-        values.next();
-        values.next();
-        values.next();
-    } else {
-        if let Some(relocations) = values.next() {
-            *avr += relocations.parse::<usize>().unwrap();
-        }
-        if let Some(g_lb) = values.next() {
-            if g_lb == "None" {
-                unreachable!()
-            } else {
-                let number = g_lb
-                    .chars()
-                    .take(g_lb.len() - 1)
-                    .skip(5)
-                    .collect::<String>()
-                    .parse::<usize>()
-                    .unwrap();
-                *gap_lb += number;
-            }
-        }
-        if let Some(g_ub) = values.next() {
-            if g_ub == "None" {
-                *fail_count += 1;
-            } else {
-                let number = g_ub
-                    .chars()
-                    .take(g_ub.len() - 1)
-                    .skip(5)
-                    .collect::<String>()
-                    .parse::<usize>()
-                    .unwrap();
-                *gap_ub += number;
-            }
-        }
-    }
-    *nodes += values.next().unwrap().parse::<u128>().unwrap();
-    *cached += values.next().unwrap().parse::<u128>().unwrap();
+fn convert(directoriy: std::path::PathBuf) -> ([usize; 5], String) {
+    println!(
+        "{}",
+        directoriy.clone().into_os_string().into_string().unwrap()
+    );
+    let filename = format!("{}", directoriy.file_name().unwrap().to_str().unwrap());
+    let paras = directoriy
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .split('_')
+        .collect::<Vec<_>>();
+    (
+        [
+            paras[0].parse::<usize>().unwrap(),
+            paras[1].parse::<usize>().unwrap(),
+            paras[2].parse::<usize>().unwrap(),
+            paras[3].parse::<usize>().unwrap(),
+            paras[4].parse::<usize>().unwrap(),
+        ],
+        filename,
+    )
 }
 
 // run `cargo run --release DIRECTORY CASES`
@@ -81,79 +34,124 @@ fn main() -> std::io::Result<()> {
         println!("{:?}", args);
         std::process::exit(1);
     }
-    let size = args[2].parse::<usize>().unwrap();
     let mut directories: Vec<std::path::PathBuf> = Vec::new();
-    for item in std::fs::read_dir(format!("../results/{}", args[1])).unwrap() {
-        directories.push(item.unwrap().path());
-        println!("{}", item.unwrap().path());
+    let mut files = Vec::new();
+    for item in std::fs::read_dir(format!("../{}", args[1]))? {
+        directories.push(item?.path());
+        files.push(convert(directories[directories.len() - 1].clone()));
     }
-
-    let mut opt = 0usize;
-    let mut infeasible = 0usize;
-    let mut avr = 0usize;
-    let mut sum = 0u128;
-    let mut max = 0u128;
-    let mut gap_lb = 0usize;
-    let mut gap_ub = 0usize;
-    let mut nodes = 0u128;
-    let mut cached = 0u128;
-    let mut oom = 0usize;
-    let mut fail_count = 0usize;
-
-    for i in 0..size {
-        let path = format!("{}/result{}", &args[1], i);
-        let mut file = File::open(&path)?;
+    files.sort();
+    // let mut file = File::create(format!("../{}/table", args[1]))?;
+    let mut buf = String::new();
+    buf.push_str(&format!("S & H & T  & N  & R  & oom & opt & inf & ave & max & re & gap_l & gap_u & node & cache \\\\ \\hline\n"));
+    //   3 & 3 & 20 & 10 & 10 & 0   & 0   & 0   & 0   & 0   & 0  & 0      & 0      & 0    & 0     \\ \hline
+    for (params, filename) in files {
+        println!("{:?}", params);
+        println!("{}", filename);
+        buf.push_str(&format!(
+            "  {} & {} & {} & {} & {} & ",
+            params[0], params[1], params[2], params[3], params[4]
+        ));
+        let mut file = File::open(&format!("../{}/{}/benchmark", args[1], filename))?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        let values: std::str::SplitWhitespace = contents.split_whitespace();
-        for e in values.clone() {
-            println!("{:?}", e);
-        }
-        calc(
-            values,
-            &mut opt,
-            &mut infeasible,
-            &mut avr,
-            &mut sum,
-            &mut max,
-            &mut gap_lb,
-            &mut gap_ub,
-            &mut nodes,
-            &mut cached,
-            &mut oom,
-            &mut fail_count,
-        );
-    }
-
-    let mut file = File::create(format!("{}/benchmark", args[1]))?;
-    let mut buf = String::new();
-    buf.push_str(&format!("{} cases\n", size));
-    if opt == 0 {
-        buf.push_str("opt: 0\n");
-    } else {
-        let gap_lb_avr = if opt == infeasible {
-            0.0
-        } else {
-            gap_lb as f64 / (opt - infeasible) as f64
-        };
-        let gap_ub_avr = if opt == infeasible + fail_count {
-            0.0
-        } else {
-            gap_lb as f64 / (opt - infeasible - fail_count) as f64
-        };
+        let mut element = contents.split_whitespace();
+        element.next();
+        element.next();
+        element.next();
+        println!();
+        let oom = element.next().unwrap();
+        let oom = oom
+            .chars()
+            .take(oom.len() - 1)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        println!("oom:: {}", oom);
+        element.next();
+        let opt = element.next().unwrap();
+        let opt = opt
+            .chars()
+            .take(opt.len() - 1)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        println!("opt:: {}", opt);
+        element.next();
+        let ife = element.next().unwrap();
+        let ife = ife
+            .chars()
+            .take(ife.len() - 1)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        println!("ife:: {}", ife);
+        element.next();
+        element.next();
+        element.next();
+        element.next();
+        let avr = element.next().unwrap().parse::<usize>().unwrap();
+        println!("avr:: {}", avr);
+        element.next();
+        element.next();
+        let re = element.next().unwrap().parse::<f64>().unwrap();
+        println!("re:: {}", re);
+        element.next();
+        element.next();
+        let max = element.next().unwrap();
+        let max = max
+            .chars()
+            .take(max.len() - 3)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        println!("max:: {}", max);
+        element.next();
+        let gap_l = element.next().unwrap();
+        let gap_l = gap_l
+            .chars()
+            .take(gap_l.len() - 1)
+            .collect::<String>()
+            .parse::<f64>()
+            .unwrap();
+        println!("gap_l:: {}", gap_l);
+        element.next();
+        let gap_u = element.next().unwrap();
+        let gap_u = gap_u
+            .chars()
+            .take(gap_u.len() - 1)
+            .collect::<String>()
+            .parse::<f64>()
+            .unwrap();
+        println!("gap_u:: {}", gap_u);
+        element.next();
+        let node = element.next().unwrap();
+        let node = node
+            .chars()
+            .take(node.len() - 1)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        println!("node:: {}", node);
+        element.next();
+        let cached = element.next().unwrap().parse::<usize>().unwrap();
+        println!("cached:: {}", cached);
+        println!();
+        // "oom & opt & inf & ave & max & re & gap_l & gap_u & node & cache \\\\ \\hline\n";
         buf.push_str(&format!(
-            "oom: {}, opt: {}, ife: {}, sum: {} ms, avr: {} ms, re: {} times, max: {}ms, gap_lb: {}, gap_ub: {}, nodes: {}, cached: {}\n",
+            "{} & {} & {} & {:.2} & {:.2} & {:.2} & {:.3} & {:.3} & {:.1} & {:.1} \\\\ \\hline\n",
             oom,
-            opt,infeasible,
-            sum,
-            sum / opt as u128,
-            avr as f64 / (opt - infeasible) as f64,
-            max,
-            gap_lb_avr,
-            gap_ub_avr,
-            nodes / opt as u128,
-            cached / opt as u128,
+            opt,
+            ife,
+            avr as f64 / 1000.0,
+            max as f64 / 1000.0,
+            re,
+            gap_l,
+            gap_u,
+            node as f64 / 10000.0,
+            cached as f64 / 10000.0
         ));
     }
+    let mut file = File::create("target/table")?;
     file.write_all(buf.as_bytes())
 }
